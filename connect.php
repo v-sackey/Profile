84836ef226fbd4fec46eaa41abf840f0dc565a08
v-sackey <118315087+v-sackey@users.noreply.github.com>
2024-01-13 07:36:03 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,2 +1,22 @@
 <?php
+$NAME = $_POST['Name'];
+$EMAIL = $_POST['Email']; 
+$Yourmessage = $_POST['Message'];   
+
+// Database Connection
+$conn = new mysqli('localhost','root', '', 'website');
+if($conn->connect_error ){
+    die('Connection Failed : '.$conn->connect_error);
+} 
+else{
+    echo "connectio established";
+    // 
+    // $stmt = $conn->prepare("insert into contact right(Name, Email, Message)
+    // values(?,?,?)");
+    // $stmt->bind_param("sss", $Name, $Email, $Message);
+    // $stmt->execute();
+    // echo "Message received";
+    // $stmt-> close();
+    // $conn->close();
+} 
 ?>
